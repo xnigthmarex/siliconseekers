6300bd92c1271741dbffcc2ac48e591935ad49bd
Anish <ani.bellamkonda@gmail.com>
2024-03-02 10:03:31 -0600

    teded page

---
+++ src/app/components/TedEd.js
@@ -0,0 +1,14 @@
+"use client";
+import React, { startTransition } from "react";
+
+export default function Teded() {
+  
+
+  return (
+    <div className="absolute">
+      <button className="pt-20" onClick={() => start()}>
+        TedEd
+      </button>
+    </div>
+  );
+}
