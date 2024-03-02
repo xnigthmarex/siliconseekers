7416948e964214b0c4428fc834dcb74b6361338a
Aryan Singh <135762633+xnigthmarex@users.noreply.github.com>
2024-03-02 16:45:24 -0600

    black

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,13 @@
 "use client";
 import React from "react";
 import { useRef } from "react";
-import auth from "./ServerActions";
+import Link from "next/link";
 
 export default function Home() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   
-  const SignInHandler = (e) => {
-    event.preventDefault();
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
-    
-  }
+  
   
   return (
     <div className="h-screen w-screen">
@@ -70,21 +65,10 @@ export default function Home() {
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                        required
-                      />
+                      
                     </div>
                     <div className="ml-3 text-sm">
-                      <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
-                      >
-                        Remember me
-                      </label>
+                      
                     </div>
                   </div>
                   <a
@@ -94,14 +78,15 @@ export default function Home() {
                     Forgot password?
                   </a>
                 </div>
-                <button
-              
+                <Link href = "/dashboard">
+                <label
+                  
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-blue-700"
-                  onClick={SignInHandler}
+                  href = "/dashboard"
                 >
                   Sign in with Email
-                </button>
-                
+                </label>
+                </Link>
 
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                   Don’t have an account yet?{" "}
